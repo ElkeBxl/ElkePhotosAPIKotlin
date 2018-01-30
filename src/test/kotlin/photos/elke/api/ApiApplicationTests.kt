f6b2c3d3c7371840caecd93b6d0dbbb5f90a1c0b
Elke Heymans <elke@Elkes-MBP-2.lan>
2018-01-30 21:04:23 +0100

    First commit after Spring Initializr

---
+++ src/test/kotlin/photos/elke/api/ApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package photos.elke.api
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
